1c4fd47172d75cf1b0038bce4b952edba4130152
Harold <harol.mont97@gmail.com>
2020-03-28 18:31:10 -0500

    Falta corregir error calcular solo simbolos

--- components/Calculadora.js
+++ components/Calculadora.js
@@ -5,28 +5,50 @@ class Calculadora extends React.Component {
     state = {
         res: ""
     }
+
     onDigit = (digit) => {
-        const expresion = this.state.res + digit
-        this.setState({
-            res: expresion
-        })
+        let arrOperacion = this.state.res.toString()
+        console.log(arrOperacion)
+        if (isNaN(digit)) {
+            arrOperacion = arrOperacion.split("")
+            let ultimoCaracterOperacion = arrOperacion[arrOperacion.length - 1]
+
+            if (ultimoCaracterOperacion !== digit && isNaN(ultimoCaracterOperacion)) {
+                arrOperacion.pop()
+                arrOperacion.push(digit)
+                const expresion = arrOperacion.join("")
+
+                this.setState({ res: expresion })
+            } else {
+                const expresion = this.state.res + digit
+
+                this.setState({ res: expresion })
+            }
+
+
+        }
+        else if (!isNaN(digit)) {
+            const expresion = this.state.res + digit
+
+            this.setState({ res: expresion })
+        }
+
     }
-    ejecutarOpercacion =()=>{
-        const resultados= eval(this.state.res)
-        this.setState({
-            res:resultados
-        })
+    ejecutarOpercacion = () => {
+
+        const resultados = eval(this.state.res)
+
+        this.setState({ res: resultados })
     }
-    // style={{ marginTop: "25px" }}
     render() {
         return (
             <div>
-                <div className="z-depth-2 lime darken-2 white-text center-align resultados" style={{ marginTop: "25px" }}>
+                <div className="z-depth-2 lime darken-2 white-text center-align resultados" >
                     {this.state.res || "0"}
                 </div>
                 <div className="calc-container lime lighten-1 hoverable z-depth-2">
                     {
-                        ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "."].map(digit => {
+                        ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"].map(digit => {
                             return (
                                 <a key={digit} className="waves-effect lime darken-3 btn" onClick={() => this.onDigit(digit)}>{digit}</a>
                             )
@@ -41,7 +63,7 @@ class Calculadora extends React.Component {
                                 )
                             })
                         }
-                        <a className="waves-effect lime darken-3 btn" onClick={() => this.setState({res:""})}>C</a>
+                        <a className="waves-effect lime darken-3 btn" onClick={() => this.setState({ res: "" })}>C</a>
                     </div>
                     <a className="waves-effect lime darken-3 btn" onClick={this.ejecutarOpercacion}>ENTER</a>
 
@@ -50,13 +72,14 @@ class Calculadora extends React.Component {
                 <style jsx>
                     {`
                     .resultados{
-                        width:205px;
+                        width:275px;
                         margin:0 auto;
                         font-size: 32px;
+                        margin-top:10px;
                         margin-bottom:10px;
                     }
                     .calc-container{
-                        width:205px;
+                        width:275px;
                         margin:0 auto;
                         text-align:center;
                         padding:12px;
